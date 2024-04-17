9fdb97eda8069806ee721b9aed9f5630689faf12
Daniel Fildán <fildand@centrum.cz>
2024-04-17 14:51:41 +0200

    Will need this

---
+++ src/main/java/net/danygames2014/examplemod/event/RecipeListener.java
@@ -0,0 +1,11 @@
+package net.danygames2014.examplemod.event;
+
+import net.mine_diver.unsafeevents.listener.EventListener;
+import net.modificationstation.stationapi.api.event.recipe.RecipeRegisterEvent;
+
+public class RecipeListener {
+    @EventListener
+    public void registerRecipes(RecipeRegisterEvent event){
+
+    }
+}
