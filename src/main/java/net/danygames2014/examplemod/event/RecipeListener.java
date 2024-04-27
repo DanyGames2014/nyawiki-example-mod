bb3bda5b3bc441cfe58c1f9eedab0e1642b05e3e
Daniel Fildán <fildand@centrum.cz>
2024-04-27 11:13:22 +0200

    Update Recipe Listener

--- src/main/java/net/danygames2014/examplemod/event/RecipeListener.java
+++ src/main/java/net/danygames2014/examplemod/event/RecipeListener.java
@@ -1,11 +1,33 @@
 package net.danygames2014.examplemod.event;
 
+import net.danygames2014.examplemod.ExampleMod;
 import net.mine_diver.unsafeevents.listener.EventListener;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.modificationstation.stationapi.api.event.recipe.RecipeRegisterEvent;
+import net.modificationstation.stationapi.api.recipe.CraftingRegistry;
+import net.modificationstation.stationapi.api.recipe.SmeltingRegistry;
 
 public class RecipeListener {
     @EventListener
-    public void registerRecipes(RecipeRegisterEvent event){
+    public void registerRecipes(RecipeRegisterEvent event) {
+        // Getting the Event type
         RecipeRegisterEvent.Vanilla type = RecipeRegisterEvent.Vanilla.fromType(event.recipeId);
+
+        if (type == RecipeRegisterEvent.Vanilla.CRAFTING_SHAPED) {
+            // This will only fire for Shaped Crafting Recipes
+            CraftingRegistry.addShapedRecipe(new ItemStack(Item.BONE, 42), "owo", "w w", "owo", 'o', new ItemStack(ExampleMod.exampleItem), 'w', new ItemStack(Block.DIAMOND_BLOCK));
+        }
+
+        if (type == RecipeRegisterEvent.Vanilla.CRAFTING_SHAPELESS) {
+            // This will only fire for Shapeless Crafting Recipes
+            CraftingRegistry.addShapelessRecipe(new ItemStack(Item.PAPER, 14), Item.APPLE, Block.DIAMOND_BLOCK);
+        }
+
+        if (type == RecipeRegisterEvent.Vanilla.SMELTING) {
+            // This will only fire for Smelting Recipes
+            SmeltingRegistry.addSmeltingRecipe(new ItemStack(Item.FLINT), new ItemStack(Block.BROWN_MUSHROOM, 8));
+        }
     }
 }
