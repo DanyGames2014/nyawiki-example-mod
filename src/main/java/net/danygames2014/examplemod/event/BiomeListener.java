efeffbf625e5c1406d478ecd747d18bf5ee0acde
Daniel Fildán <fildand@centrum.cz>
2024-10-17 09:52:58 +0200

    Biome Listener

---
+++ src/main/java/net/danygames2014/examplemod/event/BiomeListener.java
@@ -0,0 +1,11 @@
+package net.danygames2014.examplemod.event;
+
+import net.mine_diver.unsafeevents.listener.EventListener;
+import net.modificationstation.stationapi.api.event.world.biome.BiomeRegisterEvent;
+
+public class BiomeListener {
+    @EventListener
+    public void registerBiomes(BiomeRegisterEvent event){
+        
+    }
+}
